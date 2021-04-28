47ff3d255062cac7d77ba2876dd771aa1184a1af
Fernando Barra <fernandobarra2207@gmail.com>
2021-04-28 03:20:54 -0600

    Checks for constans and predicates files

--- src/components/FileUpload.vue
+++ src/components/FileUpload.vue
@@ -3,10 +3,12 @@
     <div class="constants-upload">
       <h3>Constants</h3>
       <input type="file" id="constants-file"/>
+      <p v-if="noConstantsFile">You must upload a file with your constants</p>
     </div>
     <div class="predicates-upload">
       <h3>Predicates</h3>
       <input type="file" id="predicates-file"/>
+      <p v-if="noPredicatesFile">You must upload a file with your predicates</p>
     </div>
     <button v-on:click="readFiles">Generate Form</button>
   </div>
@@ -15,10 +17,32 @@
 <script>
 export default {
   name: "FileUpload",
+  data: function() {
+    return {
+      noConstantsFile: false,
+      noPredicatesFile: false,
+    };
+  },
   methods: {
     readFiles() {
       const constantsFile = document.getElementById("constants-file").files[0];
       const predicatesFile = document.getElementById("predicates-file").files[0];
+
+      if (constantsFile === undefined){
+        this.noConstantsFile = true;
+      } else {
+        this.noConstantsFile = false;
+      }
+
+      if (predicatesFile === undefined){
+        this.noPredicatesFile = true;
+      } else {
+        this.noPredicatesFile = false;
+      }
+
+      if (constantsFile === undefined || predicatesFile === undefined) {
+        return;
+      }
 
       const readLines = (contents) => {
         const lines = contents
