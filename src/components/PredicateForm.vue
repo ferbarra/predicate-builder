892d3c920283e54b9f45b6d31ec1f1eda0342135
Fernando Barra <fernandobarra2207@gmail.com>
2021-05-02 20:57:47 -0600

    Creates component
    This components allows the user to specify which predicates
    are to be applied to which constants.

---
+++ src/components/PredicateForm.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="predicate-form">
+    <table>
+      <tr>
+        <th></th>
+        <th v-for="(predicate, index) in predicates" v-bind:key="index">{{ predicate }}</th>
+      </tr>
+      <tr v-for="(constant, index) in constants" v-bind:key="index">
+        <td>{{ constant }}</td>
+        <td v-for="(_, index2) in predicates" v-bind:key="index2">
+          <input type="checkbox" v-model="matrix[index][index2]"/>
+        </td>
+      </tr>
+    </table>
+    <button v-on:click="generateFile">Go</button>
+    <p v-for="(result, index) in results" v-bind:key="index">{{ result }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PredicateForm',
+  props: ['constants', 'predicates'],
+  data: function () {
+    return {
+      results: [],
+    };
+  },
+  computed: {
+    matrix: function() {
+      const matrix = [];
+        for (let i = 0; i < this.constants.length; ++i) {
+          const row = [];
+          for (let j = 0; j < this.predicates.length; ++j) {
+            row.push(false);
+          }
+          matrix.push(row);
+        }
+      return matrix;
+    }
+  },
+  methods: {
+    generateFile: function() {
+      const results = [];
+      for (let i = 0; i < this.predicates.length; ++i) {
+        const predicate = this.predicates[i];
+        for (let j = 0; j < this.constants.length; ++j) {
+          if (!this.matrix[j][i]) continue;
+          const constant = this.constants[j];
+          results.push(`${predicate}(${constant}).`);
+        }
+      }
+      this.results = results;
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
